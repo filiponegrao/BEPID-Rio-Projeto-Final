4afd597c20cd6cf6025ecf0695aff34d0733fe38
filiponegrao <filiposilver@hotmail.com>
2015-09-24 19:25:59 -0300

    PFFile

--- FFVChat/FFVChat/ImageEdition.swift
+++ FFVChat/FFVChat/ImageEdition.swift
@@ -27,3 +27,11 @@ class ImageEdition
     
     
 }
+
+extension UIImage {
+    var highestQualityJPEGNSData:NSData { return UIImageJPEGRepresentation(self, 1.0)! }
+    var highQualityJPEGNSData:NSData    { return UIImageJPEGRepresentation(self, 0.75)!}
+    var mediumQualityJPEGNSData:NSData  { return UIImageJPEGRepresentation(self, 0.5)! }
+    var lowQualityJPEGNSData:NSData     { return UIImageJPEGRepresentation(self, 0.25)!}
+    var lowestQualityJPEGNSData:NSData  { return UIImageJPEGRepresentation(self, 0.0)! }
+}
