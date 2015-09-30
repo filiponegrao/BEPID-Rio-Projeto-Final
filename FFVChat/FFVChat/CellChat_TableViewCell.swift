4b1e6d3675b5b6c7b10a5bac60c3804dd3a12a1a
Fernanda Carvalho <fernanda.bepid@gmail.com>
2015-09-30 16:26:46 -0300

    chat table view ahá

---
+++ FFVChat/FFVChat/CellChat_TableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CellChat_TableViewCell.swift
+//  FFVChat
+//
+//  Created by Fernanda Carvalho on 30/09/15.
+//  Copyright © 2015 FilipoNegrao. All rights reserved.
+//
+
+import UIKit
+
+class CellChat_TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
