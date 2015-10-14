90c79cbaea6a79a85b8c4273556e5a9505a42ad5
Fernanda Carvalho <fernanda.bepid@gmail.com>
2015-10-14 15:12:44 -0300

    receiver profile test

--- FFVChat/FFVChat/ReceiverProfile_ViewController.swift
+++ FFVChat/FFVChat/ReceiverProfile_ViewController.swift
@@ -8,15 +8,30 @@
 
 import UIKit
 
-class ReceiverProfile_ViewController: UIViewController {
+class ReceiverProfile_ViewController: UIViewController
+{
+    
+    @IBOutlet weak var contactImage: UIImageView!
+    
+    var contact : Contact!
+    
 
-    override func viewDidLoad() {
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
+        
+        self.contactImage.layer.cornerRadius = self.contactImage.frame.size.height/2
+        self.contactImage.clipsToBounds = true
+        self.contactImage.image = DAOUser.sharedInstance.getProfileImage()
+        self.contactImage.layer.borderWidth = 1.5
+        self.contactImage.layer.borderColor = UIColor.whiteColor().CGColor
+        
 
         // Do any additional setup after loading the view.
     }
 
-    override func didReceiveMemoryWarning() {
+    override func didReceiveMemoryWarning()
+    {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
