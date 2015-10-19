583360fe2e447a2bc8a61fcbfa2777953ad232f4
filiponegrao <filiposilver@hotmail.com>
2015-10-19 02:22:55 -0200

    altas modificações

--- FFVChat/FFVChat/DAOFriendRequests.swift
+++ FFVChat/FFVChat/DAOFriendRequests.swift
@@ -7,14 +7,14 @@
 //
 
 import Foundation
-import Parse
-
 
 enum requestNotification : String
 {
     case requestsLoaded
     
     case reloadRequest
+    
+    case friendAdded
 }
 
 
@@ -69,54 +69,22 @@ class DAOFriendRequests
     
     func loadRequests()
     {
-        var requests = [FriendRequest]()
-        let query = PFQuery(className: "FriendRequest")
-        query.whereKey("target", equalTo: DAOUser.sharedInstance.getUserName())
-        query.whereKey("status", equalTo: "Pendente")
-        query.findObjectsInBackgroundWithBlock { ( objects:[AnyObject]?, error: NSError?) -> Void in
-            if let objects = objects as? [PFObject]
+        DAOParse.getRequests { (requests) -> Void in
+            
+            if(requests.count > 0)
             {
-                for object in objects
-                {
-                    requests.append(FriendRequest(sender: object.valueForKey("sender") as! String, target: DAOUser.sharedInstance.getUserName()))
-                    
-                    if(object == objects.last)
-                    {
-                        self.requests = requests
-                        print("\(self.requests.count) requisicoes de amizade foram carregadas para o usuario atual")
-                        NSNotificationCenter.defaultCenter().postNotificationName(requestNotification.requestsLoaded.rawValue, object: nil)
-                    }
-                }
+                self.requests = requests
+                NSNotificationCenter.defaultCenter().postNotificationName(requestNotification.requestsLoaded.rawValue, object: nil)
             }
-            self.requests = requests
+            
         }
-        self.requests = requests
     }
     
     
     func acceptRequest(request: FriendRequest)
     {
-        let query = PFQuery(className: "FriendRequest")
-        query.whereKey("sender", equalTo: request.sender)
-        query.whereKey("target", equalTo: DAOUser.sharedInstance.getUserName())
-        query.findObjectsInBackgroundWithBlock { (objects: [AnyObject]?, error: NSError?) -> Void in
-            
-            print("Recebidos \(objects?.count) requests")
-            if let objects = objects as? [PFObject]
-            {
-                for object in objects
-                {
-                    object.setValue("Aceito", forKey: "status")
-                    DAOContacts.addContactByUsername(request.sender, callback: { (success, error) -> Void in
-                        if(success == true)
-                        {
-                            print("Convite de amizade de \(request.sender) aceito e adicionado como contato")
-                            self.updateObject(object)
-                        }
-                    })
-                }
-            }
-            
+        DAOParse.acceptRequestOnParse(request) { (success, error) -> Void in
+            NSNotificationCenter.defaultCenter().postNotificationName(requestNotification.friendAdded.rawValue, object: nil)
         }
     }
     
@@ -127,111 +95,22 @@ class DAOFriendRequests
      */
     func friendsAccepted()
     {
-        let query = PFQuery(className: "FriendRequest")
-        query.whereKey("sender", equalTo: DAOUser.sharedInstance.getUserName())
-        query.findObjectsInBackgroundWithBlock { (objects: [AnyObject]?, error: NSError?) -> Void in
-            
-            if let objects = objects as? [PFObject]
-            {
-                for object in objects
-                {
-                    let status = object.valueForKey("status") as! String
-                    if(status == "Aceito")
-                    {
-                        let contact = object.valueForKey("target") as! String
-                        DAOContacts.addContactByUsername(contact, callback: { (success, error) -> Void in
-                            if(success == true)
-                            {
-                                print("adicionando \(contact) por ter aceitado o pedido de amizade")
-                                object.deleteEventually()
-                            }
-                        })
-                    }
-                }
-            }
-            
-        }
+        DAOParse.finalizeRequests()
     }
     
     
     func sendRequest(username: String)
     {
-        let query = PFUser.query()
-        query?.whereKey("username", equalTo: username)
-        query?.getFirstObjectInBackgroundWithBlock({ (object: PFObject?, error: NSError?) -> Void in
-            if(object != nil)
-            {
-                let request = PFObject(className: "FriendRequest")
-                request["sender"] = DAOUser.sharedInstance.getUserName()
-                request["target"] = username
-                request["status"] = "Pendente"
-                request.saveEventually({ (success : Bool, error: NSError?) -> Void in
-                    if(success == true)
-                    {
-                        self.sendPushForFriendRequest(username)
-                        print("Convite de amizade enviado para \(username)")
-                    }
-                })
-
-            }
-            
-        })
+        DAOParse.sendFriendRequest(username)
+        DAOParse.sendPushFriendRequest(username)
     }
     
-    func sendPushForFriendRequest(username: String)
-    {
-        let message = "\(DAOUser.sharedInstance.getUserName()) quer lhe adicionar como um contato"
-        
-        let data = [ "title": "Convite de amizade no FFVChat",
-            "alert": message, "badge": 1, "do": appNotification.friendRequest.rawValue]
-        
-        print("enviando notificacao")
-        let userQuery = PFUser.query()
-        userQuery?.whereKey("username", equalTo: username)
-        
-        // Find devices associated with these users
-        let pushQuery = PFInstallation.query()
-        pushQuery!.whereKey("user", matchesQuery: userQuery!)
-        
-        // Send push notification to query
-        let push = PFPush()
-        push.setQuery(pushQuery) // Set our Installation query
-        push.setData(data as [NSObject : AnyObject])
-        push.sendPushInBackground()
-    }
-    
-    func updateObject(object: PFObject)
-    {
-        object.saveEventually({ (success: Bool, error: NSError?) -> Void in
-            if(success != true)
-            {
-                self.tried = 0
-                self.loadRequests()
-            }
-            else if(self.tried < 10)
-            {
-                self.tried++
-                self.updateObject(object)
-            }
-        })
-    }
     
     func wasAlreadyRequested(username: String, callback: (was: Bool) -> Void) -> Void
     {
-        let query = PFQuery(className: "FriendRequest")
-        query.whereKey("sender", equalTo: DAOUser.sharedInstance.getUserName())
-        query.whereKey("target", equalTo: username)
-        query.getFirstObjectInBackgroundWithBlock { (object: PFObject?, error: NSError?) -> Void in
+        DAOParse.checkUserAlreadyRequested(username) { (was) -> Void in
             
-            if(object != nil)
-            {
-                callback(was: true)
-            }
-            else
-            {
-                callback(was: false)
-            }
-            
+            callback(was: was)
         }
     }
         
---
+++ FFVChat/FFVChat/TheJudger.swift
@@ -0,0 +1,71 @@
+//
+//  TheJudger.swift
+//  FFVChat
+//
+//  Created by Filipo Negrao on 18/10/15.
+//  Copyright © 2015 FilipoNegrao. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import Photos
+
+private let data : TheJudger = TheJudger()
+
+class TheJudger : NSObject
+{
+    
+    override init()
+    {
+        super.init()
+        
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "judge", name: UIApplicationUserDidTakeScreenshotNotification, object: nil)
+    }
+    
+    func inicializandoJudger()
+    {
+        print("Judger Pronto!")
+    }
+    
+    class var Singleton : TheJudger
+    {
+        return data
+    }
+    
+
+    func judge()
+    {
+        NSNotificationCenter.defaultCenter().postNotificationName(appNotification.trustLevelChanged.rawValue, object: nil)
+        DAOParse.decreaseTrustLevel()
+        self.deletePhoto()
+    }
+    
+    
+    func deletePhoto()
+    {
+        let fetchOptions: PHFetchOptions = PHFetchOptions()
+        
+        fetchOptions.sortDescriptors = [NSSortDescriptor(key: "creationDate", ascending: true)]
+        
+        let fetchResult = PHAsset.fetchAssetsWithMediaType(PHAssetMediaType.Image, options: fetchOptions)
+        
+        if (fetchResult.lastObject != nil) {
+            
+            let lastAsset: PHAsset = fetchResult.lastObject as! PHAsset
+            
+            let arrayToDelete = NSArray(object: lastAsset)
+            
+            PHPhotoLibrary.sharedPhotoLibrary().performChanges( {
+                PHAssetChangeRequest.deleteAssets(arrayToDelete)},
+                completionHandler: {
+                    success, error in
+                    NSLog("Finished deleting asset. %@", (success ? "Success" : error!))
+            }) 
+            
+            
+            
+        }
+    }
+}
+
+
