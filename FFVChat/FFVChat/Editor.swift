f8cb99589d8e3969d4faf1ed0324d07a08655bab
filiponegrao <filiposilver@hotmail.com>
2015-10-27 17:26:58 -0200

    unblur circle

---
+++ FFVChat/FFVChat/Editor.swift
@@ -0,0 +1,49 @@
+//
+//  Editor.swift
+//  FFVChat
+//
+//  Created by Filipo Negrao on 27/10/15.
+//  Copyright © 2015 FilipoNegrao. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Editor
+{
+    class func circleUnblur(image: UIImage, x: CGFloat, y: CGFloat) -> UIImage
+    {
+        var prop : CGFloat!
+        
+        if(image.size.width > image.size.height)
+        {
+            if(image.size.width > screenWidth)
+            {
+                prop = screenWidth/image.size.width
+            }
+            else
+            {
+                prop = screenWidth/image.size.width
+            }
+        }
+        else if(image.size.height > image.size.width)
+        {
+            if(image.size.height > screenHeight)
+            {
+                prop = screenHeight/image.size.height
+            }
+            else
+            {
+                prop = screenHeight/image.size.height
+            }
+        }
+        
+        UIGraphicsBeginImageContext(CGSizeMake(diametro, diametro))
+        image.drawInRect(CGRectMake(-x, -y, image.size.width * prop, image.size.height * prop), blendMode: CGBlendMode.Normal, alpha: 1)
+        
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        return image
+    }
+}
