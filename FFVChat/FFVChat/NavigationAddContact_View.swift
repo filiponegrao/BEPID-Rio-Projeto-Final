9b7179e32afebc18baa17169016cd44ff0a80f56
Fernanda Carvalho <fernanda.bepid@gmail.com>
2015-11-15 04:10:44 -0200

    add contact coisinhas essa search n muda de cor nemfu

---
+++ FFVChat/FFVChat/NavigationAddContact_View.swift
@@ -0,0 +1,52 @@
+//
+//  NavigationAddContact_View.swift
+//  FFVChat
+//
+//  Created by Fernanda Carvalho on 15/11/15.
+//  Copyright © 2015 FilipoNegrao. All rights reserved.
+//
+
+import UIKit
+
+class NavigationAddContact_View: UIView
+{
+
+    var viewController : AddContact_ViewController!
+    
+    var backButton : UIButton!
+    
+    var tittle : UILabel!
+    
+    init(requester: AddContact_ViewController)
+    {
+        self.viewController = requester
+        super.init(frame: CGRectMake(0, 0, screenWidth, 60))
+        self.backgroundColor = oficialDarkGray
+        
+        self.backButton = UIButton(frame: CGRectMake(0, 15, 45, 45))
+        self.backButton.setImage(UIImage(named: "backButton"), forState: .Normal)
+        self.backButton.addTarget(self, action: "back", forControlEvents: UIControlEvents.TouchUpInside)
+        self.addSubview(backButton)
+        
+        self.tittle = UILabel(frame: CGRectMake(0, 20, screenWidth, 35))
+        self.tittle.text = "Add contact"
+        self.tittle.textAlignment = .Center
+        self.tittle.textColor = oficialGreen
+        self.tittle.font = self.tittle.font
+            .fontWithSize(30)
+        self.addSubview(tittle)
+    }
+    
+    required init?(coder aDecoder: NSCoder)
+    {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func back()
+    {
+        self.viewController.navigationController?.popViewControllerAnimated(true)
+    }
+    
+    
+
+}
