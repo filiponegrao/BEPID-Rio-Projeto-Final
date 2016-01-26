0c59be88c6c879adefe99ba807bded4ae0a3efa9
Fernanda Carvalho <fernanda.bepid@gmail.com>
2016-01-26 00:53:00 -0200

    MessageAlertSettings_VC (configurações pra notificação de mensagens)

---
+++ FFVChat/FFVChat/MessageAlertSettings_ViewController.swift
@@ -0,0 +1,57 @@
+//
+//  MessageAlertSettings_ViewController.swift
+//  FFVChat
+//
+//  Created by Fernanda Carvalho on 26/01/16.
+//  Copyright © 2016 FilipoNegrao. All rights reserved.
+//
+
+import UIKit
+
+class MessageAlertSettings_ViewController: UIViewController
+{
+    var tableView : UITableView!
+    
+    var navBar : UIView!
+    
+    var tittle : UILabel!
+    
+    var backButton : UIButton!
+
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        
+        self.view.backgroundColor = oficialMediumGray
+        
+        self.navBar = UIView(frame: CGRectMake(0, 0, screenWidth, 70))
+        self.navBar.backgroundColor = oficialDarkGray
+        self.view.addSubview(self.navBar)
+        
+        self.tittle = UILabel(frame: CGRectMake(0, 25, screenWidth, 35))
+        self.tittle.text = "Notifications"
+        self.tittle.font = UIFont(name: "Sukhumvit Set", size: 22)
+        self.tittle.textAlignment = .Center
+        self.tittle.textColor = oficialGreen
+        self.view.addSubview(self.tittle)
+        
+        
+        
+
+
+    }
+
+    override func didReceiveMemoryWarning()
+    {
+        super.didReceiveMemoryWarning()
+
+    }
+    
+    override func viewDidLayoutSubviews()
+    {
+        self.tittle.setSizeFont(22)
+    }
+    
+
+
+}
